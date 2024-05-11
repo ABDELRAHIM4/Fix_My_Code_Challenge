7d71eb7c2e139a7bda4c9dae95d5ed437ef021d3
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-05-11 10:32:25 -0700

    challenge

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """class square"""
-    def __init__(self, width, height):
+    def __init__(self, width = 0, height = 0):
         self.width = width
         self.height = height
 
@@ -17,12 +17,12 @@ class Square:
 
 
     def __str__(self):
-        return ("{}/{}".format(self.width, self.height))
+        return ("{}/{}".format(self.width, self.width))
 
 
 if __name__ == "__main__":
 
-    s = Square(width=12, height=9)
+    s = Square(width=12)
     print(s)
     print(s.area())
     print(s.Perimeter())
